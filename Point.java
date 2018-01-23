d2309f0abe0732bff2e2e23cf9f465d47bfe2739
red2fred2 <admin@red2fred2.com>
2018-01-23 14:20:39 -0600

    added point class

---
+++ Point.java
@@ -0,0 +1,16 @@
+class Point {
+   
+   private int x;
+   private int y;
+   
+   //constructor
+   public Point() {
+      this.x = 0;
+      this.y = 0;
+   }
+   
+   public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+   }
+}
