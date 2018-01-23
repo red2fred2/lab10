0b2e657157f076bee7601d3659b6cf2c21de8720
red2fred2 <admin@red2fred2.com>
2018-01-23 14:33:16 -0600

    AHHHHH

--- Rectangle.java
+++ Rectangle.java
@@ -1,7 +1,24 @@
 class Rectangle extends DrawableObject {
    
+   int leftX;
+   int topY;
+   int rightX;
+   int bottomY;
+   int width;
+   int height;
+   
    //constructor
-   public Rectangle(Graphics g) {
+   public Rectangle(Graphics g, Point topLeft, Point bottomRight) {
       
+   }
+   
+   protected rect(Point topLeft, Point bottomRight) {
+      final int leftX = topLeft.getX();
+      final int topY = topLeft.getY();
+      
+      final int width = ;
+      final int height = bottomY - topY;
+      
+      g.drawRect(leftX, topY, );
    }
 }
