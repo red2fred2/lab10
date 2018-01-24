c802ffba9aa54ed43f40cbe13fdc8b2da790e193
red2fred2 <admin@red2fred2.com>
2018-01-23 22:32:11 -0600

    got rid of useless methods and added polygon class

---
+++ Polygon.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+
+public class Polygon extends DrawableObject {
+
+   protected Point[] points;
+   
+   public Polygon(Graphics graphics) {
+      super(graphics);
+   }
+
+}
