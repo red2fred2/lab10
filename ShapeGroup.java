8e48629ba51bf9a6df89a5aaf6f66466dc0256f0
red2fred2 <admin@red2fred2.com>
2018-01-23 23:42:03 -0600

    finished shape group

--- ShapeGroup.java
+++ ShapeGroup.java
@@ -1,9 +1,31 @@
 public class ShapeGroup {
+
+   Rectangle[] rectangles;
+   Circle[] circles;
+   Line[] lines;
+   Polygon[] polygons;
+
+   private void draw(Rectangle rect) {rect.draw();}
+   private void draw(Circle circ) {circ.draw();}
+   private void draw(Line lin) {lin.draw();}
+   private void draw(Polygon poly) {poly.draw();}
    
-   DrawableObject[] shapes;
+   private void fill(Rectangle rect) {rect.fill();}
+   private void fill(Circle circ) {circ.fill();}
+   private void fill(Polygon poly) {poly.fill();}
    
-   public ShapeGroup(DrawableObject... shapes) {
-      this.shapes = shapes;
+   protected void drawAll() {
+      for(Rectangle rect : rectangles) draw(rect);
+      for(Circle circ : circles) draw(circ);
+      for(Line lin : lines) draw(lin);
+      for(Polygon poly : polygons) draw(poly);
+   }
+   
+   protected void fillAll() {
+      for(Rectangle rect : rectangles) fill(rect);
+      for(Circle circ : circles) fill(circ);
+      for(Line lin : lines) draw(lin);
+      for(Polygon poly : polygons) fill(poly);
    }
    
 }
