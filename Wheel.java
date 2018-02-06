a237f1d2c007b5c140c07c061f70a6da79153ad1
3061700 <3061700@smsd.org>
2018-02-06 14:28:29 -0600

    okay so this  just adds in wheel

--- Wheel.java
+++ Wheel.java
@@ -1,6 +1,12 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
-class Wheel extends DrawableObject {
+class Wheel extends Circle {
 
+final private int diameter = 40;
+
+public Wheel(Graphics g, Point topLeft) {
+
+   super(g, topLeft.getX(), topLeft.getX(), );
+}
 }
