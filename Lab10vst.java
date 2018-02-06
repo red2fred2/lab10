6b8a8af64220429817c7f24bc0f6812a9dc79718
red2fred2 <admin@red2fred2.com>
2018-02-06 14:07:10 -0600

    got rid of head

--- Lab10vst.java
+++ Lab10vst.java
@@ -5,7 +5,6 @@ import java.applet.*;
 public class Lab10vst extends Applet {
 	public void paint(Graphics g) {
        
-       Head head = new Head(g, new Point(0, 0), new Point(200, 200));
-       head.draw();
+       
    }
 }
