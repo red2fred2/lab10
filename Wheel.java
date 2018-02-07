b9a23275b84f4a67add60ff0b34f4088ae41262c
3061700 <3061700@smsd.org>
2018-02-07 14:21:06 -0600

    a

--- Wheel.java
+++ Wheel.java
@@ -8,6 +8,6 @@ final private int diameter = 40;
 public Wheel(Graphics g, Point topLeft) {
 
    super(g, topLeft.getX(), topLeft.getY(), topLeft.getX() + diameter, topLeft.getY() + diameter);
-   super.setColor(Color.grey)
+   super.setColor(Color.gray);
 }
 }
