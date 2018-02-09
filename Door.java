af69f25b29308a1cb0cc057d816c74fe1fd378b4
red2fred2 <admin@red2fred2.com>
2018-02-09 14:29:57 -0600

    polygon in door

--- Door.java
+++ Door.java
@@ -1,10 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 class Door {
    
    protected Graphics g;
    private Point topLeft;
+   private Polygon[] shape;
    
    public Door(Graphics g, Point topLeft) {
       this.g = g;
       this.topLeft = topLeft;
+      this.shape = new Polygon(g, [Point(0, 0), Point(100, 100), Point(100, 0), Point(0, 0)], Color.red);
+   }
+   
+   public void draw() {
+      shape.fill()
    }
 }
