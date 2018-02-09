4cc9d0d3a8ad8ead43c823a4b302a845677d0eef
red2fred2 <admin@red2fred2.com>
2018-02-09 14:14:01 -0600

    puch

--- Wheel.java
+++ Wheel.java
@@ -3,16 +3,14 @@ import java.awt.Graphics;
 
 class Wheel extends Circle {
 
-final private int diameter = 40;
+   final private int diameter = 40;
 
-public Wheel(Graphics g, Point topLeft) {
-
-   super(g, topLeft, new Point(topLeft.getX() + 50, topLeft.getY() + 50));
-   super.setColor(Color.gray);
-   
-}
-   
-      public void draw() {
-      super.fill();
-}
+   public Wheel(Graphics g, Point topLeft) {
+      super(g, topLeft, new Point(topLeft.getX() + 50, topLeft.getY() + 50));
+      super.setColor(Color.gray);
+      LugNut leftNut = new LugNut(g, topLeft);
+   }
+   public void draw() {
+      super.fill();  
+   }
 }
