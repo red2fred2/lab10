5dd06ade1085d97cbe0795112b0dbdbfd512e4c5
red2fred2 <admin@red2fred2.com>
2018-02-12 20:51:17 -0600

    commented the point class

--- lab10/Point.java
+++ lab10/Point.java
@@ -1,26 +1,39 @@
 package lab10;
+
 /**
  * An x, y point
  */
 class Point {
 
+   /** The X component of the point */
    private int x;
+   /** The Y component of the point */
    private int y;
 
-   public Point() {
-      this.x = 0;
-      this.y = 0;
-   }
-
+   /**
+    * Creates a point from X and Y components
+    * @param x
+    *   The X component of the point
+    * @param y
+    *   The Y component of the point
+    */
    public Point(int x, int y) {
       this.x = x;
       this.y = y;
    }
 
+   /**
+    * @return
+    *   X component of the point
+    */
    public int getX() {
       return this.x;
    }
 
+   /**
+    * @return
+    *   Y component of the point
+    */
    public int getY() {
       return this.y;
    }
