2ee7629c82bd3a50460e43ae88578451c8913343
red2fred2 <admin@red2fred2.com>
2018-02-12 21:19:23 -0600

    door setup

--- lab10/Door.java
+++ lab10/Door.java
@@ -1,14 +1,26 @@
 package lab10;
 import java.awt.Graphics;
 
+/**
+ * Represents a car door
+ */
 class Door {
    
+   /** specific graphics object */
    protected Graphics graphics;
    
+   /**
+    * Constructs a Door object
+    * @param graphics
+    *   specific graphics object
+    */
    public Door(Graphics graphics) {
       this.graphics = graphics;
    }
    
+   /**
+    * Draws this car door
+    */
    public void draw() {
       
    }
