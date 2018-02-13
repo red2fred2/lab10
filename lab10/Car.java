ed3fcda1f61724d1d77d0d099a6b09ef3d84d5ab
red2fred2 <admin@red2fred2.com>
2018-02-12 20:27:24 -0600

    commented car

--- lab10/Car.java
+++ lab10/Car.java
@@ -1,14 +1,29 @@
+//package stuff
 package lab10;
 import java.awt.Graphics;
 
+/**
+ * Represents a car
+ */
 public class Car {
    
-   Body body;
+   //decalare variables
+   private Body body;
+   protected Point position;
    
-   public Car(Graphics g) {
-      this.body = new Body(g, new Point(100, 100));
+   /**
+    * @param graphics
+    *   The graphics class to be used
+    * @param position
+    *   the top left corner of the car's rectangular bounding box
+    */
+   public Car(Graphics graphics, Point position) {
+      this.body = new Body(graphics, new Point(100, 100));
    }
    
+   /**
+    * Draws the car
+    */
    public void draw() {
       
    }
--- lab10/Lab10vst.java
+++ lab10/Lab10vst.java
@@ -6,6 +6,14 @@ import java.applet.*;
 
 public class Lab10vst extends Applet {
 	public void paint(Graphics g) {
+	   
+	   //declare variables
+	   final Point carPosition = new Point(100, 100);
+	   
+	   //initialize objects
+       Car car = new Car(g, carPosition);
        
+       //do the thing
+       car.draw();
    }
 }
