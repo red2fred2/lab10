d9adfba2b205d12b2aa6ff8afb796d5d79cc74a2
red2fred2 <admin@red2fred2.com>
2018-02-12 20:33:59 -0600

    commented body

--- lab10/Body.java
+++ lab10/Body.java
@@ -1,16 +1,29 @@
 package lab10;
 import java.awt.Graphics;
 
+/**
+ * Represents the body of a car
+ */
 public class Body {
 
-   protected Graphics g;
+   /** The graphics object to be used */
+   protected Graphics graphics;
+   /** The general shape of the body */
    private Polygon shape;
 
-   public Body(Graphics g, Point topLeft) {
-
-      this.g = g;
+   /**
+    * @param graphics
+    *   The graphics object to be used
+    * @param topLeft
+    *   the top left corner of the body's rectangular bounding box
+    */
+   public Body(Graphics graphics, Point position) {
+      this.graphics = graphics;
    }
-
+   
+   /**
+    * Draws the body
+    */
    public void Draw() {
       shape.fill();
    }
