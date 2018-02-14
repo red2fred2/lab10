4d1f24be436afad80b591dc8b9d69705a457946e
3061700 <3061700@smsd.org>
2018-02-14 14:38:33 -0600

    okay

--- lab10/Lab10vst.java
+++ lab10/Lab10vst.java
@@ -8,12 +8,15 @@ public class Lab10vst extends Applet {
 	public void paint(Graphics g) {
 	   
 	   //declare variables
-	   final Point carPosition = new Point(100, 100);
-	   
+	   final Point car1Position = new Point(100, 100);
+	   final Point car2Position = new Point(400, 100);
+      
 	   //initialize objects
-       Car car = new Car(g, carPosition, new Color(200,5,15 ));
+       Car car1 = new Car(g, car1Position, new Color(200,5,15 ));
+       Car car2 = new Car(g, car2Position, new Color(15,5,200 ));
        
        //do the thing
-       car.draw();
+       car1.draw();
+       car2.draw();
    }
 }
